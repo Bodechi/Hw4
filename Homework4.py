a7f3596c71b3a288a25238ea8c1a4aa537f56a59
Bodechi <Fosa9301@mail.ru>
2024-06-16 23:47:52 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,8 @@
+#my_string = input("Почему не читают коментарии под моим HW?")
+#print(len("Почему не читают коментарии под моим HW?"))
+my_string = "Почему не читают коментарии под моим HW?"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
